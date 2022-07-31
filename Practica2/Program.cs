77cb241c06cce4885c5f7c75781b4b58d6a584fc
MINEDUCYT <MINEDUCYT@DESKTOP-MT9DF42>
2022-07-30 21:45:23 -0600

    Agregar archivos de proyecto.

---
+++ Practica2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int Edad = 19;
+            string name = "Fernando";
+
+            Console.WriteLine("Su Nombre es: " + name + "y su edad es: ", +Edad); 
+        }
+    }
+}
